bcc3a0c6b1894ad069fdbd5a79273f33ae2fd75d
Easd99 <solanoae@uninorte.edu.co>
2022-07-15 15:51:43 -0500

    first commit

---
+++ db/migrate/20220715203957_create_texts.rb
@@ -0,0 +1,10 @@
+class CreateTexts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :texts do |t|
+      t.string :title
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
